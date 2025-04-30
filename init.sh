31ea649170280f271b03ae5f1d0d6af1c13cb044
wesjos <142285064+nonwesjoe@users.noreply.github.com>
2025-04-30 21:41:53 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -24,22 +24,28 @@ mv -vf resolv.conf /etc/resolv.conf
 mv -vf hosts /etc/hosts
 mv -vf locale.conf /etc/locale.conf
 
-echo "next"
-
-sed -i '/CheckSpace/d' /etc/pacman.conf
-
-rm -r /etc/pacman.d/gnupg
-pacman-key --init
-pacman-key --populate archlinuxarm
-pacman-key --refresh-keys
-
 groupadd -g 3003 aid_inet
 groupadd -g 3004 aid_net_raw
 groupadd -g 1003 aid_graphics
+usermod -g 3003 -G 3003,3004 -a _apt
 usermod -G 3003 -a root
 
-echo '-------------update---------------'
-pacman -Syu
+sed -i '$ a\max ALL=(ALL:ALL) ALL' /etc/sudoers
+sed -i '$ a\deb http://ftp.de.debian.org/debian sid main' /etc/apt/sources.list
+sed -i '$ a\deb http://ftp.us.debian.org/debian sid main' /etc/apt/sources. list
 
-echo 'run pacman -Syu (if fail try again and again)'
-echo 'remember to install neofetch later'
+apt update
+apt upgrade
+apt install -y wget nano neofetch sudo git net-tools
+echo "next"
+
+groupadd storage
+groupadd wheel
+useradd -m -g users -G wheel,audio,video,storage,aid_inet -s /bin/bash max
+echo "SET YOUR PASSWORD"
+passwd max
+
+apt update
+apt install neofetch
+
+echo done
